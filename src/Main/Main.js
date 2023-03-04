d6807667827aa53c089ef91031a974a846f2ad57
synchoz <dima2191@gmail.com>
2023-03-04 18:50:07 +0200

    push for a little update

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,17 +4,70 @@ import Navbar from '../Navigation/Navbar'
 
 export default function Main() {
     return (
-        <div className='headerImage'>
-            <Navbar />
-            <div className="bottomHeaderInfo">
-                <div>
-                    <div className="font-bold">Web Services</div>
-                    <p>some text with a header like achieve greatness with out app some motivation sort pf</p>
+        <div>
+            <div className='headerImage'>
+                <Navbar />
+                <div className="bottomHeaderInfo">
+                    <div>
+                        <div className="font-bold">Web Services</div>
+                        <p>some text with a header like achieve greatness with out app some motivation sort pf</p>
+                    </div>
+                </div>
+                {/* <div className='aboutInfo bg-white h-2/6'>
+
+                </div> */}
+            </div>
+            <div className='mainSectionInfo bg-white border-t-[3px]'>
+                {/* <div className='infoSection grid sm:max-2xl:grid-cols-4 sm:grid-cols-2 gap-4 h-full'> */}
+                <div className='infoSection grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 h-full'>
+                {/* grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 */}
+                    <div className='InfoPart text-center col-span-1 m-auto'>{/* change it later for a component! */}
+                        <div className='infoImageSection h-32 flex justify-center items-center py-1'>
+                            <div className='infoImage rounded-full border-black border bg-red-500 h-full w-2/5'></div>
+                        </div>
+                        <div className='infoText'>
+                            <div className='font-bold'>Weight Logging</div>
+                            <div>This app offers extensive weight logging including dates reps etc...</div>
+                        </div>
+                    </div>
+                    <div className='InfoPart text-center col-span-1 m-auto'>{/* change it later for a component! */}
+                        <div className='infoImageSection h-32 flex justify-center items-center py-1'>
+                            <div className='infoImage rounded-full border-black border bg-red-500 h-full w-2/5'></div>
+                        </div>
+                        <div className='infoText'>
+                            <div className='font-bold'>Weight Logging</div>
+                            <div>This app offers extensive weight logging including dates reps etc...</div>
+                        </div>
+                    </div>
+                    <div className='InfoPart text-center col-span-1 m-auto'>{/* change it later for a component! */}
+                        <div className='infoImageSection h-32 flex justify-center items-center py-1'>
+                            <div className='infoImage rounded-full border-black border bg-red-500 h-full w-2/5'></div>
+                        </div>
+                        <div className='infoText'>
+                            <div className='font-bold'>Weight Logging</div>
+                            <div>This app offers extensive weight logging including dates reps etc...</div>
+                        </div>
+                    </div>
+                    <div className='InfoPart text-center col-span-1 m-auto'>{/* change it later for a component! */}
+                        <div className='infoImageSection h-32 flex justify-center items-center py-1'>
+                            <div className='infoImage rounded-full border-black border bg-red-500 h-full w-2/5'></div>
+                        </div>
+                        <div className='infoText'>
+                            <div className='font-bold'>Weight Logging</div>
+                            <div>This app offers extensive weight logging including dates reps etc...</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {/* <div className='aboutInfo bg-white h-2/6'>
-
-            </div> */}
+            <div className='subMainSectionInfo bg-slate-300 border-t-[3px]'>
+                <div className='subMainSectionHeader'>
+                    <div className='mt-[40px]'>
+                        <div className='w-3/4 m-auto font-bold'>Title</div>
+                        <div className='w-3/4 m-auto mt-2'>ext ever sier took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem</div>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     )
 }
